ed8b276dc0c8fefa195a9eaca3a2154a8cae2ce7
Andy Jiang <andyjiang916@gmail.com>
2022-06-26 14:31:05 -0500

    This is the initial check-in.

---
+++ BioLabTimer/BioLabTimer/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace BioLabTimer;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ BioLabTimer/BioLabTimer/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace BioLabTimer;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
